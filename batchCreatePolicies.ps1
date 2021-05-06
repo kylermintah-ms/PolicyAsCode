2b6c7d743b6a8c85950479def0bec10387aa91c8
Kyler Mintah <kylermintah@microsoft.com>
2021-05-06 14:27:18 +0000

    Updated batchCreatePolicies.ps1

--- batchCreatePolicies.ps1
+++ batchCreatePolicies.ps1
@@ -1,4 +1,4 @@
-$policyDefRootFolder = "$env:System_DefaultWorkingDirectory"+"/policies"
+$policyDefRootFolder = "$env:System_DefaultWorkingDirectory"+"/samples"
 $subscriptionName = "$($env:subscriptionName)"
 
 class PolicyDef {
