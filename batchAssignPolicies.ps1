37833de23e0d1940541b44c82bb1c47dfcd2a4eb
Kyler Mintah <kylermintah@microsoft.com>
2021-05-06 14:33:34 +0000

    Updated batchAssignPolicies.ps1

--- batchAssignPolicies.ps1
+++ batchAssignPolicies.ps1
@@ -1,6 +1,6 @@
 $policyObjs = ConvertFrom-Json -InputObject $env:POLICYDEFS
 $policyAssignmentRG = "$(resourceGroupName)$(Release.EnvironmentName)"
-$policyDefRootFolder = "$env:System_DefaultWorkingDirectory" + "/samples"
+$policyDefRootFolder = "$env:System_DefaultWorkingDirectory"
 
 foreach ($policyDefFolder in (Get-ChildItem -Path $policyDefRootFolder -Directory)) {
 
